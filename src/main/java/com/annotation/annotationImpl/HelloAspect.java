ad7ee21fe99484aeb5ba9e6c252d4c491517cb45
Pinshuducha <java_zhoulu@163.com>
2019-02-14 16:50:21 +0800

    加上注释

--- src/main/java/com/annotation/annotationImpl/HelloAspect.java
+++ src/main/java/com/annotation/annotationImpl/HelloAspect.java
@@ -9,18 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class HelloAspect {
+    // 将此类的解析指向注解
     @Pointcut("@annotation(com.annotation.annotationImpl.WoToken)")
     private void cut() {
         //切点
     }
     @Around("cut()&&@annotation(woToken)")
     public Object advice(ProceedingJoinPoint joinPoint, WoToken woToken) throws Throwable {
+        // 获取连接点方法运行时的入参列表
         Object[] args = joinPoint.getArgs();
         for (int i= 0;i<args.length;i++) {
             String argValue = (String) args[i];
             String s = argValue + woToken.value();
             args[i] = s;
         }
+        //执行
         Object proceed = joinPoint.proceed(args);
         return proceed;
     }
